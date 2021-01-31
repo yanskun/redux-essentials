b68547bc042369a97b8694a9ef5b7680e77ad4a5
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-02-01 02:04:27 +0900

    feat: add user

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -23,6 +24,7 @@ export const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        {post.user && <PostAuthor userId={post.user} />}
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
